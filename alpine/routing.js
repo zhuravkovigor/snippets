b953a40ee54395949f66aacf68337667fc35ea92
Zhuravkov Igor <zhuravkovigor@icloud.com>
2024-12-20 14:33:25 +0500

    Implement client-side routing with caching and history management

---
+++ alpine/routing.js
@@ -0,0 +1,58 @@
+document.addEventListener("alpine:init", () => {
+  const cache = {};
+  const cacheTimers = {};
+
+  const router = (event) => {
+    event = event || window.event;
+    event.preventDefault();
+    window.history.pushState({}, "", event.target.href);
+
+    handleLocation();
+  };
+
+  const routes = {
+    "/": "/pages/index.html",
+    404: "/pages/error.html",
+  };
+
+  const handleLocation = async () => {
+    const path = window.location.pathname;
+    const route = routes[path] || routes[404];
+
+    Alpine.store("pageName", path);
+
+    if (cache[path]) {
+      document.getElementById("content").innerHTML = cache[path];
+      resetCacheTimer(path);
+    } else {
+      const html = await fetch(route).then((data) => data.text());
+      cache[path] = html;
+      document.getElementById("content").innerHTML = html;
+      setCacheTimer(path);
+    }
+  };
+
+  const setCacheTimer = (path) => {
+    if (cacheTimers[path]) {
+      clearTimeout(cacheTimers[path]);
+    }
+    cacheTimers[path] = setTimeout(() => {
+      delete cache[path];
+      delete cacheTimers[path];
+    }, 2 * 60 * 1000); // 2 minutes
+  };
+
+  const resetCacheTimer = (path) => {
+    clearTimeout(cacheTimers[path]);
+    setCacheTimer(path);
+  };
+
+  addEventListener("popstate", handleLocation);
+  window.router = router;
+
+  handleLocation();
+});
+
+// example use
+// <a href="/" onclick="router()">Home</a>
+/* <a href="/about" onclick="router()">About</a> */
